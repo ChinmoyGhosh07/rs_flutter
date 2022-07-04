a1002e2436afce7c43896c29f7426130f65fbb70
cghosh@sastasundar.com <cghosh@sastasundar.com>
2022-07-04 10:40:53 +0530

    full code upload

--- lib/login/login_page.dart
+++ lib/login/login_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'package:retailershakti_flutter/utils/routes.dart';
 
@@ -12,6 +13,22 @@ class _LoginPageState extends State<LoginPage> {
   String name = "";
   bool changeButton = false;
   final _formKey = GlobalKey<FormState>();
+  int group1Value = 0;
+  int group2Value = 0;
+
+  TextEditingController phone = TextEditingController(text: "");
+  TextEditingController pass = TextEditingController(text: "");
+  bool _checkValue = true;
+
+  submit(){
+    if(_checkValue && phone.text.isNotEmpty){
+      //Navigator.of(context).push(MaterialPageRoute(builder: (BuildContext context) => const OtpScreen()));
+    }
+    if(!_checkValue && phone.text.isNotEmpty && pass.text.isNotEmpty){
+      //Navigator.of(context).push(MaterialPageRoute(builder: (BuildContext context) => const Home()));
+    }
+
+  }
 
   moveToHome(BuildContext context) async{
     var state = _formKey.currentState;
@@ -37,13 +54,18 @@ class _LoginPageState extends State<LoginPage> {
           key: _formKey,
           child: Column(
             children: [
-              Image.asset("assets/images/login.png",
-                fit: BoxFit.fill,
-              ),
               SizedBox(
                 height: 20,
               ),
-              Text("Welcome $name",
+              Image.asset("assets/images/logo_big.png",
+                fit: BoxFit.fitHeight,
+                width: 400,
+                height: 100,
+              ),
+              SizedBox(
+                height: 8,
+              ),
+              /*Text("Welcome $name",
                 style: TextStyle(
                   fontSize: 28,
                   fontWeight: FontWeight.bold,
@@ -51,14 +73,50 @@ class _LoginPageState extends State<LoginPage> {
               ),
               SizedBox(
                 height: 20,
-              ),
+              ),*/
               Padding(padding: const EdgeInsets.symmetric(vertical: 16,horizontal: 32),
                 child: Column(
                   children: [
+                    Row(
+                      children: [
+                        Radio(
+                          value: 1,
+                          groupValue: group1Value,
+                          onChanged: (value) {
+                            setState(() {
+                              group1Value = 1;
+                            });
+                          },
+                        ),
+                        Expanded(child: Text("Existing User")),
+                        Radio(
+                          value: 2,
+                          groupValue: group2Value,
+                          onChanged: (value) {
+                            setState(() {
+                              group2Value = 2;
+                            });
+                          },
+                        ),
+                        Expanded(child: Text("New User")),
+                      ],
+                    ),
                     TextFormField(
+                      controller: phone,
+                      keyboardType: TextInputType.phone,
+                      maxLines: 1,
+                      inputFormatters: <TextInputFormatter>[
+                    // for below version 2 use this
+                    FilteringTextInputFormatter.allow(RegExp(r'[0-9]')),
+                    LengthLimitingTextInputFormatter(10),
+                    // for version 2 and greater youcan also use this
+                    FilteringTextInputFormatter.digitsOnly
+
+                  ],
                       decoration: InputDecoration(
                         hintText: "Enter mobile number",
                         labelText: "Mobile number",
+
                       ),
                       validator: (value) {
                         if("" == value){
@@ -71,7 +129,20 @@ class _LoginPageState extends State<LoginPage> {
                         setState(() {});
                       },
                     ),
+                    !_checkValue ?
                     TextFormField(
+                        controller: pass,
+                        decoration: const InputDecoration(
+                          labelText: "Password",
+                          hintText: "Enter your Password",
+                        ),
+                        validator: (_){
+                          if(pass.text.isEmpty) {
+                            return "Enter a valid password";
+                          }
+                        }
+                    ) : const Text(""),
+                    /*TextFormField(
                       obscureText: true,
                       decoration: InputDecoration(
                         hintText: "Enter OTP",
@@ -84,14 +155,44 @@ class _LoginPageState extends State<LoginPage> {
                         }
                         return null;
                       },
-                    )
+                    ),*/
+                    Row(
+                      children: [
+                        Checkbox(value: _checkValue, onChanged: (bool? newValue) {
+                          setState(() {
+                            _checkValue = newValue!;
+                          });
+                        },),//onChanged: onChangeCheck(checkValue)),
+                        const Text("Login with OTP"),
+                      ],
+                    ),
+                    Text("terms and condition",
+                      textAlign: TextAlign.left,
+                      style: TextStyle(
+                        fontSize: 14,
+                        fontWeight: FontWeight.normal,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 4,
+                    ),
+                    Row(
+                      children: [
+                        Checkbox(value: _checkValue, onChanged: (bool? newValue) {
+                          setState(() {
+                            _checkValue = newValue!;
+                          });
+                        },),//onChanged: onChangeCheck(checkValue)),
+                        const Text("WhatApp consent"),
+                      ],
+                    ),
                   ],
                 ),
               ),
               SizedBox(
-                height: 20,
+                height: 8,
               ),
-              Material(
+              /*Material(
                 color: Colors.deepPurple,
                 borderRadius:
                     BorderRadius.circular(changeButton?50:8),
@@ -117,18 +218,8 @@ class _LoginPageState extends State<LoginPage> {
                     ),
                   ),
                 ),
-              )
-
-              /*ElevatedButton(
-                onPressed: () {
-                  print("Hi RS user");
-                  Navigator.pushNamed(context, MyRoutes.homeRoute);
-                },
-                style: TextButton.styleFrom(minimumSize: Size(200,40)),
-                child: Text("Login"),
-
               )*/
-
+              ElevatedButton(onPressed: submit, child: const Text("Submit"))
 
             ],
           ),
